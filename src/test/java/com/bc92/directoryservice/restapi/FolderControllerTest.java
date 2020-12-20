2205cf31a4982dc37f4d765066cee73cf6420520
briancollins-92 <bcollins.sw@gmail.com>
2020-12-20 18:41:27 +0000

    demo commit

---
+++ src/test/java/com/bc92/directoryservice/restapi/FolderControllerTest.java
@@ -0,0 +1,13 @@
+package com.bc92.directoryservice.restapi;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class FolderControllerTest {
+
+  @Test
+  void test() {
+    fail("Not yet implemented");
+  }
+
+}
